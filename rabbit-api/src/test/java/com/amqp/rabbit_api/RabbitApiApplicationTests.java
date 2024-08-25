06ba8078e372554b4db3662fb9166b99c1515a58
fmedeiro <fmedeiros13@yahoo.com.br>
2024-08-25 12:23:36 -0300

    First version on master...

---
+++ rabbit-api/src/test/java/com/amqp/rabbit_api/RabbitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amqp.rabbit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
